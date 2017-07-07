5d25faccaaa7bf6e3c18b3b75d17e2c6fbbf5e61
Zombienaute <Zombienaute@1036766.no-reply.drupal.org>
2017-07-07 17:36:50 +0200

    Issue #2886636 by acbramley, drunken monkey, andypost, borisson_: Added a dedicated "search_api_autocomplete" form element.

--- src/AutocompleteFormUtility.php
+++ src/AutocompleteFormUtility.php
@@ -2,32 +2,12 @@
 
 namespace Drupal\search_api_autocomplete;
 
-use Drupal\Core\Render\RendererInterface;
-use Drupal\search_api_autocomplete\Controller\AutocompleteController;
-
 /**
  * Provides some helper methods to deal with the autocomplete form.
  *
  * @todo This should be a service.
  */
 class AutocompleteFormUtility {
-
-  /**
-   * The renderer.
-   *
-   * @var \Drupal\Core\Render\RendererInterface
-   */
-  protected $renderer;
-
-  /**
-   * Creates a new AutocompleteFormUtility instance.
-   *
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   The renderer.
-   */
-  public function __construct(RendererInterface $renderer) {
-    $this->renderer = $renderer;
-  }
 
   /**
    * Split a string with search keywords into two parts.
@@ -65,62 +45,10 @@ class AutocompleteFormUtility {
    * @param array $fields
    *   (optional) Used fulltext fields.
    */
-  public function alterElement(array &$element, SearchInterface $search, array $fields = []) {
-    // @todo find a cleaner solution.
-    $controller = new AutocompleteController(\Drupal::service('renderer'));
-    if ($controller->access($search, \Drupal::currentUser())->isAllowed()) {
-      // Add option defaults (in case of updates from earlier versions).
-      $options = $search->getOptions() + [
-        'autosubmit' => TRUE,
-        'delay' => 0,
-        'min_length' => 1,
-        'submit_button_selector' => ':submit',
-      ];
-
-      $fields_string = $fields ? implode(',', $fields) : '-';
-
-      $autocomplete_route_name = 'search_api_autocomplete.autocomplete';
-      $autocomplete_route_parameters = [
-        'search_api_autocomplete_search' => $search->id(),
-        'fields' => $fields_string,
-      ];
-
-      $js_settings = [];
-      if ($options['submit_button_selector'] != ':submit') {
-        $js_settings['selector'] = $options['submit_button_selector'];
-      }
-      if ($options['delay']) {
-        $js_settings['delay'] = $options['delay'];
-      }
-      if ($options['autosubmit']) {
-        $js_settings['auto_submit'] = TRUE;
-      }
-      if ($options['min_length'] > 1) {
-        $js_settings['min_length'] = $options['min_length'];
-      }
-
-      // Allow overriding of the default handler with a route.
-      if ($callback = $search->getOption('custom_autocomplete_url_callback')) {
-        $callback_options = $search->getOption('custom_autocomplete_url_options', []);
-        /** @var \Drupal\Core\Url $url */
-        $url = call_user_func($callback, $this, $element, $callback_options);
-        $autocomplete_route_name = $url->getRouteName();
-        $autocomplete_route_parameters = $url->getRouteParameters();
-        $js_settings['custom_path'] = TRUE;
-      }
-      $element['#attached']['library'][] = 'search_api_autocomplete/search_api_autocomplete';
-      if ($js_settings) {
-        $element['#attached']['drupalSettings'] = [
-          'search_api_autocomplete' => [
-            $search->id() => $js_settings,
-          ],
-        ];
-      }
-
-      $element['#autocomplete_route_name'] = $autocomplete_route_name;
-      $element['#autocomplete_route_parameters'] = $autocomplete_route_parameters;
-      $element['#attributes']['data-search-api-autocomplete-search'] = $search->id();
-    }
+  public static function alterElement(array &$element, SearchInterface $search, array $fields = []) {
+    $element['#type'] = 'search_api_autocomplete';
+    $element['#search_id'] = $search->id();
+    $element['#fields'] = $fields;
   }
 
 }
---
+++ src/Element/SearchApiAutocomplete.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Drupal\search_api_autocomplete\Element;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Render\Element\Textfield;
+use Drupal\search_api_autocomplete\Controller\AutocompleteController;
+use Drupal\search_api_autocomplete\Entity\Search;
+
+/**
+ * Provides a Search API Autocomplete form element.
+ *
+ * @FormElement("search_api_autocomplete")
+ */
+class SearchApiAutocomplete extends Textfield {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    $info = parent::getInfo();
+    $class = get_class($this);
+
+    // Apply default form element properties.
+    $info['#search_id'] = NULL;
+    $info['#fields'] = [];
+
+    array_unshift($info['#process'], [$class, 'processSearchApiAutocomplete']);
+
+    return $info;
+  }
+
+  /**
+   * Adds Search API Autocomplete functionality to a form element.
+   *
+   * @param array $element
+   *   The form element to process. Properties used:
+   *   - #search_id: The entity ID of the Search config entity.
+   *   - #fields: (optional) Fulltext fields to use as the basis for
+   *     autocompletion.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param array $complete_form
+   *   The complete form structure.
+   *
+   * @return array
+   *   The processed form element.
+   *
+   * @throws \InvalidArgumentException
+   *   Thrown if the #search_id property is missing or invalid.
+   */
+  public static function processSearchApiAutocomplete(array &$element, FormStateInterface $form_state, array &$complete_form) {
+    // Require a search ID argument. If the #search_id key is not set on the
+    // element, we can't load the Search entity so we should throw an exception.
+    if (empty($element['#search_id'])) {
+      throw new \InvalidArgumentException('Missing required "#search_id" parameter.');
+    }
+    if (!($search = Search::load($element['#search_id']))) {
+      $search_id = $element['#search_id'];
+      throw new \InvalidArgumentException("Search entity with ID \"$search_id\" not found.");
+    }
+
+    $controller = new AutocompleteController(\Drupal::service('renderer'));
+    $access = $controller->access($search, \Drupal::currentUser());
+
+    $metadata = BubbleableMetadata::createFromRenderArray($element);
+    $metadata->merge(BubbleableMetadata::createFromObject($access))
+      ->applyTo($element);
+
+    if (!$access->isAllowed()) {
+      // Don't process if access isn't allowed.
+      return $element;
+    }
+
+    // Check for passed-in full text fields.
+    $fields_string = '-';
+    if (!empty($element['#fields'])) {
+      $fields_string = implode(',', $element['#fields']);
+    }
+
+    // Add option defaults (in case of updates from earlier versions).
+    $options = $search->getOptions() + [
+      'autosubmit' => TRUE,
+      'delay' => 0,
+      'min_length' => 1,
+      'submit_button_selector' => ':submit',
+    ];
+    $js_settings = [];
+    if ($options['submit_button_selector'] != ':submit') {
+      $js_settings['selector'] = $options['submit_button_selector'];
+    }
+    if ($delay = $search->getOption('delay') !== NULL) {
+      $js_settings['delay'] = $delay;
+    }
+    if ($options['autosubmit']) {
+      $js_settings['auto_submit'] = TRUE;
+    }
+    if ($options['min_length'] > 1) {
+      $js_settings['min_length'] = $options['min_length'];
+    }
+
+    $element['#attached']['library'][] = 'search_api_autocomplete/search_api_autocomplete';
+    if ($js_settings) {
+      $element['#attached']['drupalSettings'] = [
+        'search_api_autocomplete' => [
+          $search->id() => $js_settings,
+        ],
+      ];
+    }
+
+    $element['#autocomplete_route_name'] = 'search_api_autocomplete.autocomplete';
+    $element['#autocomplete_route_parameters'] = [
+      'search_api_autocomplete_search' => $search->id(),
+      'fields' => $fields_string,
+    ];
+    $element['#attributes']['data-search-api-autocomplete-search'] = $search->id();
+
+    return $element;
+  }
+
+}
