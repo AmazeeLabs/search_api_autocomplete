ed138411b49312b6ac614e60d90d142e29ed2777
Thomas Seidl <remus@gmx.net>
2017-09-28 16:44:55 +0200

    Improved documentation for custom scripts.

--- tests/search_api_autocomplete_test/core/custom_autocomplete_script.php
+++ tests/search_api_autocomplete_test/core/custom_autocomplete_script.php
@@ -5,6 +5,14 @@
  * Outputs some autocomplete suggestions as JSON, for testing purposes.
  *
  * Contained within a /core/ sub-directory to trick Drupal's .htaccess file.
+ *
+ * The JSON output of this file is a single array of suggestion list items, with
+ * each being an object with the following keys:
+ * - value: The keywords which should be entered into the search field for this
+ *   suggestion. For returning a suggestion that will redirect to a URL instead
+ *   of entering keywords, use the URL here proceeded by a single leading space
+ *   character.
+ * - label: HTML which should be displayed for the suggestion.
  */
 
 search_api_autocomplete_test_custom_autocomplete_callback();
